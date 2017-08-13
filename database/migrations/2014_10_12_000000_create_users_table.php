2be63072e8d309c9d08c54c395b29d594208d692
darkSlayer <d97arkslayer@localhost.localdomain>
2017-08-12 22:30:41 -0500

    USER MIGRATION

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('id',40)->unique();
+            $table->string('name',60);
+            $table->string('lastname',120);
+            $table->enum('gender',['Male','Female']);
+            $table->string('telephoneNumber',20);
+            $table->string('direction',120);
+            $table->string('email',120);
+            $table->date('birthDate');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
